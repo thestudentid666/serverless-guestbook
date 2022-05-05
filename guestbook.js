7a85ecd659824fdc412f04bbc81378a5ac4c620b
TheStudentID666 <909087757@qq.com>
2022-05-05 22:01:32 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://ced8ffd9.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
